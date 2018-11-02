18df394ceab66e3fcb8de5829ec8459a69a273bc
Sam Boero <sam.l.boero@gmail.com>
2018-11-02 14:58:11 +0100

    main back end functions done

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,38 @@
 const express = require('express');
 const router  = express.Router();
+const Movie = require('../models/Movie');
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
 });
 
+router.get('/movies', (req, res) => {
+  Movie.find()
+  .then(moviesFromDb => {
+    console.log("DEBUG", moviesFromDb)
+    console.log(moviesFromDb)
+    res.render("movies", {
+      listOfMovies:moviesFromDb
+    }) 
+})
+.catch(err =>{
+  console.log("AHHHHHHHHHH")
+})
+})
+
+
+
+router.get('/movies/:id', (req, res, next) => {
+  let id = req.params.id //the id from the url
+  Movie.findById(id)
+  .then (moviesFromDb => {
+    res.render('see-more', {
+   moviesFromDb
+  })
+  })
+  .catch (error => {
+    console.log("ERROR")
+  })
+    })
 module.exports = router;
